c0c36ff8fe49b1e7740d48bd706c91006c0efb1a
Brian Kloppenborg <bkloppenborg@gmail.com>
2012-02-05 05:35:13 +0000

    Enable/Disable parameters in GUI now works.

--- src/QT/CParameterItem.cpp
+++ src/QT/CParameterItem.cpp
@@ -30,14 +30,21 @@ QVariant CParameterItem::data(int role) const
 
 void CParameterItem::setData(const QVariant & value, int role)
 {
-	// TODO: This is really hacky, and possibly can fail if a parameter is
-	// of an integer type!
-	if(role ==  Qt::EditRole)
+	// TODO: This is a really hacky way of setting the parameter values,
+	// we should find a different way of doing it?
+	switch(role)
 	{
+	case Qt::CheckStateRole:
+		// Checkbox enabled/disabled
 		if(value.type() == QVariant::Int)
 			mParent->SetFree(mID, value.toBool());
-		else if(value.type() == QVariant::Double)
+		break;
+
+	case Qt::EditRole:
+		if(value.type() == QVariant::Double)
 			mParent->SetParam(mID, float(value.toDouble()));
+		break;
+
 	}
 
 	QStandardItem::setData(value, role);
