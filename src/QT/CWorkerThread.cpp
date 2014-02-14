baddf3b731854b090446f6186f053fbca663da08
Brian Kloppenborg <bkloppenborg@gmail.com>
2014-02-14 16:43:26 -0500

    Bugfix for max_gl_layers >> max_cl_layers.

--- src/QT/CWorkerThread.cpp
+++ src/QT/CWorkerThread.cpp
@@ -155,8 +155,11 @@ void CWorkerThread::CreateGLBuffer(GLuint & FBO, GLuint & FBO_texture, GLuint & 
 	if(n_layers > max_layers)
 		n_layers = max_layers;
 
+	CheckOpenGLError("A");
 	CreateGLMultisampleRenderBuffer(mImageWidth, mImageHeight, mImageSamples, FBO, FBO_texture, FBO_depth);
+	CheckOpenGLError("B");
 	CreateGLStorageBuffer(mImageWidth, mImageHeight, n_layers, FBO_storage, FBO_storage_texture);
+	CheckOpenGLError("C");
 }
 
 void CWorkerThread::CreateGLMultisampleRenderBuffer(unsigned int width, unsigned int height, unsigned int samples,
@@ -195,17 +198,21 @@ void CWorkerThread::CreateGLStorageBuffer(unsigned int width, unsigned int heigh
 {
 	glGenFramebuffers(1, &FBO_storage);
 	glBindFramebuffer(GL_FRAMEBUFFER, FBO_storage);
+	CheckOpenGLError("aa");
 
 	glGenTextures(1, &FBO_storage_texture);
-	glBindTexture(GL_TEXTURE_2D, FBO_storage_texture);
+	glBindTexture(GL_TEXTURE_2D_ARRAY, FBO_storage_texture);
+	CheckOpenGLError("ab");
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_COMPARE_FUNC, GL_LEQUAL);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_R32F, width, height, 0, GL_RED, GL_FLOAT, NULL);
+	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_COMPARE_FUNC, GL_LEQUAL);
+	CheckOpenGLError("ay");
+	glTexImage3D(GL_TEXTURE_2D_ARRAY, 0, GL_R32F, width, height, depth, 0, GL_RED, GL_FLOAT, NULL);
+	CheckOpenGLError("az");
 
 	glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, FBO_storage_texture, 0);
 
