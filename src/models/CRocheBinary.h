a457f976640c6e2140ef5708721bb9d04f8082f8
Brian Kloppenborg <bkloppenborg@gmail.com>
2014-04-17 10:27:46 -0400

    Applied BSD/Allman style formatting (to match the rest of SIMTOI).

--- src/models/CRocheBinary.h
+++ src/models/CRocheBinary.h
@@ -11,8 +11,8 @@
 #include "CModel.h"
 #include "chealpix.h"
 
-
-class CRocheBinary: public CModel {
+class CRocheBinary: public CModel
+{
 public:
 	CRocheBinary();
 	virtual ~CRocheBinary();
@@ -28,7 +28,7 @@ protected:
 	GLuint mTextureID; // texture id	
 
 	bool mModelReady;
-	
+
 public:
 	void normalize_vert(double * vec);
 	void Render(GLuint framebuffer_object, const glm::mat4 & view);
@@ -36,21 +36,24 @@ public:
 
 	static shared_ptr<CModel> Create();
 
-	virtual string GetID() { return "roche_binary"; };
-	
+	virtual string GetID()
+	{
+		return "roche_binary";
+	}
+	;
+
 	virtual void SetShader(CShaderPtr shader); // Overrides CModel::SetShader
 
-	
-private: 
+private:
 	// Constants
-	const double AU = 1.496e11  ;// meters
+	const double AU = 1.496e11; // meters
 	const double rsun = 6.955e8; // m	
-	const double G = 6.67428e-11 ;// m3 kg-1 s-2
+	const double G = 6.67428e-11; // m3 kg-1 s-2
 	const double parsec = 3.08567758e16; // m
-	const double msun = 1.9891e30 ;// kg
+	const double msun = 1.9891e30; // kg
 	const double gmr = G * msun / rsun;
 	const double gmr2 = gmr / rsun;
-	
+
 	// Main parameters
 	double lambda; // wavelength of observation, used to convert temperatures to fluxes, in meters
 	double parallax; // distance from the sun, in pc
@@ -60,20 +63,20 @@ private:
 	double desired_resolution; // mas, Healpix resolution
 	double gravity_darkening;
 
-	double mass1 ; // mass of the Roche-lobed star
-	double mass2 ; // mass of the other star in the binary
+	double mass1; // mass of the Roche-lobed star
+	double mass2; // mass of the other star in the binary
 	double teff_pole; // temperature of the Roche-lobed star
-	
-	double massratio ;
-	double omega_rot ; // orbital period in Hz	
-	
+
+	double massratio;
+	double omega_rot; // orbital period in Hz
+
 	double separation_rsun;
-			
+
 	double npix_estimate;
 	double nside_estimate;
-    	long nside;
+	long nside;
 	long npix;
-	
+
 	long* ipix;
 	double* theta_center;
 	double* phi_center;
@@ -85,25 +88,31 @@ private:
 	double* theta_corners;
 	double* phi_corners;
 	double* radii_corners;
-	
+
 	// Spots (should be a class, really)
 	int nspots; // number of spots
-        double* spot_temperature;
+	double* spot_temperature;
 	double* spot_theta;
 	double* spot_phi;
 	double* spot_thetasize;
 	double* spot_phisize;
-	
+
 	//void normalize_vert(double * vec);
-	void triaxial_pot(double & pot, double & dpot, const double radius,  const double theta, const double phi);
-	void triaxial_gravity(double & gravity, const double radius, const double theta, const double phi);
-	void surface_radii(double* radii, const double *theta, const double *phi, const unsigned int vsize);	
-	void surface_gravity(double * gravity, const double* radii, const double* theta, const double* phi, const unsigned int vsize);
-	void surface_temperature(double* temperature, const double* gravity, const double gravity_pole, const unsigned int vsize ) ;
+	void triaxial_pot(double & pot, double & dpot, const double radius,
+			const double theta, const double phi);
+	void triaxial_gravity(double & gravity, const double radius,
+			const double theta, const double phi);
+	void surface_radii(double* radii, const double *theta, const double *phi,
+			const unsigned int vsize);
+	void surface_gravity(double * gravity, const double* radii,
+			const double* theta, const double* phi, const unsigned int vsize);
+	void surface_temperature(double* temperature, const double* gravity,
+			const double gravity_pole, const unsigned int vsize);
 	void surface_flux();
 public:
-	void GenerateRoche(vector<vec3> & vbo_data, vector<unsigned int> & elements);
-	
+	void GenerateRoche(vector<vec3> & vbo_data,
+			vector<unsigned int> & elements);
+
 };
 
 #endif /* CROCHEBINARY_H_ */
