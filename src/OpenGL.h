b53a383e8c709a4090ee091dd665c50acd4c846c
Brian Kloppenborg <bkloppenborg@gmail.com>
2014-08-19 15:58:03 -0400

    Suppress Apple OpenGL compiler warnings. We need mixed functionality.

--- src/OpenGL.h
+++ src/OpenGL.h
@@ -17,6 +17,8 @@
 
 // Standard OpenGL Headers
 #ifdef __APPLE__
+# define __gl_h_
+# define GL_DO_NOT_WARN_IF_MULTI_GL_VERSION_HEADERS_INCLUDED
 #include <OpenGL/gl3.h>
 #include <OpenGL/glu.h>
 #else
