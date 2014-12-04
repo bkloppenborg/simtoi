37818d534aa5322346663cd9a82b4c1d64556fb3
Brian Kloppenborg <brian@arrayfire.com>
2014-12-03 23:20:32 -0500

    Switch git version tag finding method.

--- CMakeModules/SetVersion.cmake
+++ CMakeModules/SetVersion.cmake
@@ -1,6 +1,6 @@
 # Create a file containing version information
 EXECUTE_PROCESS(
-    COMMAND git describe --abbrev=0 --tags
+    COMMAND git describe --tags `git rev-list --tags --max-count=1`
     WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
     OUTPUT_VARIABLE GIT_VERSION_TAG
     OUTPUT_STRIP_TRAILING_WHITESPACE
@@ -13,8 +13,8 @@ EXECUTE_PROCESS(
 )
 
 MESSAGE(STATUS "Version information for" ${PROJECT_SOURCE_DIR})
-MESSAGE(STATUS "Version tag:" ${GIT_VERSION_TAG})
-MESSAGE(STATUS "Version hash:" ${GIT_VERSION_HASH})
+MESSAGE(STATUS "Version tag: " ${GIT_VERSION_TAG})
+MESSAGE(STATUS "Version hash: " ${GIT_COMMIT_HASH})
 
 # strip the leading 'v' off of the git version tag:
 string(LENGTH ${GIT_VERSION_TAG} GIT_TAG_LENGTH)
