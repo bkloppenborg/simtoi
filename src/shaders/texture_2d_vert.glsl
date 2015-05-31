f040daece06cefe796e59a3dee9d3b543bbba899
Brian Kloppenborg <brian@kloppenborg.net>
2015-05-30 21:43:37 -0400

    Fix trailing semicolon in shaders.

--- src/shaders/texture_2d_vert.glsl
+++ src/shaders/texture_2d_vert.glsl
@@ -1,30 +1,30 @@
 #version 330 core
 
-/* 
+/*
  * Copyright (c) 2012 Brian Kloppenborg
  *
  * If you use this software as part of a scientific publication, please cite as:
  *
- * Kloppenborg, B.; Baron, F. (2012), "SIMTOI: The SImulation and Modeling 
- * Tool for Optical Interferometry" (Version X). 
+ * Kloppenborg, B.; Baron, F. (2012), "SIMTOI: The SImulation and Modeling
+ * Tool for Optical Interferometry" (Version X).
  * Available from  <https://github.com/bkloppenborg/simtoi>.
  *
- * This file is part of the SImulation and Modeling Tool for Optical 
+ * This file is part of the SImulation and Modeling Tool for Optical
  * Interferometry (SIMTOI).
- * 
+ *
  * SIMTOI is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License 
+ * it under the terms of the GNU Lesser General Public License
  * as published by the Free Software Foundation version 3.
- * 
+ *
  * SIMTOI is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public 
+ *
+ * You should have received a copy of the GNU Lesser General Public
  * License along with SIMTOI.  If not, see <http://www.gnu.org/licenses/>.
  */
- 
+
 in vec3 position;
 in vec3 normal;
 in vec3 tex_coords;
@@ -44,4 +44,4 @@ void main()
     ModelPosition = (scale * vec4(position, 1.0)).xyz;
     gl_Position = view * translation * rotation * scale * vec4(position, 1.0);
     Tex_Coords = (tex_coords).xy;
-};
+}
