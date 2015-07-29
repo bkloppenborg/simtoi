7541566032a47ef66887b4b1b3f7276be68830f7
Your Name <you@example.com>
2015-07-29 10:00:23 -0400

    Changes to the alpha disk files

---
+++ src/models/CDisk_Alpha1973.h
@@ -0,0 +1,28 @@
+/*
+ * CDisk_Alpha1973.h
+ *
+ *  Created on: July, 22, 2015
+ *      Author: j robert parks iv
+ *
+ *  A class implementing a disk model consisting of a power-law internal density
+ *  profile and an exponential taper at larger radii
+ */
+
+#ifndef CDISK_ALPHA1973_H_
+#define CDISK_ALPHA1973_H_
+
+#include "CDensityDisk.h"
+
+class CDisk_Alpha1973: public CDensityDisk
+{
+
+public:
+	CDisk_Alpha1973();
+	virtual ~CDisk_Alpha1973();
+
+	static shared_ptr<CModel> Create();
+
+	virtual string GetID() { return "disk_alpha1973"; };
+};
+
+#endif /* CDISK_ANDREWS2009_H_ */
